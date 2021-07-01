1a903821681e185d263f17705247f3126384a886
yzdann <22779039+yzdann@users.noreply.github.com>
2021-07-01 16:52:03 +0430

    Create some private subnet in VPC

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
+  region  = local.region
 }
 
 locals {
@@ -20,13 +20,20 @@ locals {
     Owner = "yazdan"
     Env   = "dev"
   }
+  region               = "us-west-2"
+  zones                = ["a", "b", "c"]
+  private_subnet_count = 3
+  cidr                 = "10.0.0.0/16"
 }
 
 module "main" {
   source = "./modules/vpc"
+  name   = local.name
+  cidr   = local.cidr
 
-  name = local.name
-  cidr = "10.0.0.0/16"
+  azs = [for zone in local.zones : "${local.region}${zone}"]
+
+  private_subnets = [for count in range(local.private_subnet_count) : cidrsubnet(local.cidr, 8, 5 * count)]
 
   tags = local.tags
 }
