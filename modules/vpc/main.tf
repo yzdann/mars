78e51a5fe49dbb0808c392aeb53d8eb68025bb05
yzdann <22779039+yzdann@users.noreply.github.com>
2021-07-03 22:21:16 +0430

    Add aws route table association in vpc modules

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -117,3 +117,10 @@ resource "aws_route" "public_internet_gateway" {
     create = "5m"
   }
 }
+
+resource "aws_route_table_association" "public" {
+  count = var.create_vpc && length(var.public_subnets) > 0 ? length(var.public_subnets) : 0
+
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public[0].id
+}
