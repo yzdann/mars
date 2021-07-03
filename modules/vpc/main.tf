becb5c83d0ec557bc7bfc6ebb0f352b10a1b8ac6
yzdann <22779039+yzdann@users.noreply.github.com>
2021-07-03 22:16:35 +0430

    Add aws route in vpc modules

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -105,3 +105,15 @@ resource "aws_route_table" "public" {
     var.public_route_table_tags,
   )
 }
+
+resource "aws_route" "public_internet_gateway" {
+  count = var.create_vpc && var.create_igw && length(var.public_subnets) > 0 ? 1 : 0
+
+  route_table_id         = aws_route_table.public[0].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this[0].id
+
+  timeouts {
+    create = "5m"
+  }
+}
