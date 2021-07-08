a9b6c839118469e01e086634448e8d6e18ff8be1
yzdann <22779039+yzdann@users.noreply.github.com>
2021-07-08 23:55:20 +0430

    Add use asg

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ locals {
   cidr                 = "10.0.0.0/16"
 }
 
-module "main" {
+module "vpc" {
   source = "./modules/vpc"
   name   = local.name
   cidr   = local.cidr
@@ -38,4 +38,22 @@ module "main" {
   public_subnets  = [for count in range(local.public_subnet_count) : cidrsubnet(local.cidr, 8, 10 + count)]
 
   tags = local.tags
+}
+
+module "asg" {
+  source               = "./modules/asg"
+  name                 = local.name
+  create_launch_config = true
+
+  image_id            = "ami-03d5c68bab01f3496"
+  instance_type       = "t2.micro"
+  user_data           = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8000 & 
+              EOF
+  vpc_zone_identifier = module.vpc.private_subnets
+  min_size            = 2
+  max_size            = 5
+  health_check_type   = "ELB"
 }
